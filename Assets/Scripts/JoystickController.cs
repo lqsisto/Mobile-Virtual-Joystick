d0f83aa12b13fb29d98cc87170165beb714158fc
lqsisto <laurimikael.kuusisto@gmail.com>
2020-03-21 18:36:18 +0200

    Delete unnecessary files

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -6,35 +6,38 @@ using UnityEngine.UI;
 
 public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
-    private Image _BGImage;
-    private Image _joystickImage;
-    private Vector3 _inputVector;
+    [SerializeField] private Image bGImage;
+    [SerializeField] private Image joystickImage;
+    private Vector3 inputVector;
 
     public static bool JoystickPressed;
-
-    void Start ()
-    {
-        _BGImage = transform.Find ("JoystickBG").GetComponent<Image> ();
-
-        _joystickImage = transform.Find ("JoystickFG").GetComponent<Image> ();
-    }
+    
+    
     public virtual void OnDrag (PointerEventData pod)
     {
         Vector2 pos;
+        var bgImageSizeDelta = bGImage.rectTransform.sizeDelta;
 
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle (_BGImage.rectTransform,
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle (bGImage.rectTransform,
                 pod.position,
                 pod.pressEventCamera,
                 out pos))
         {
-            pos.x = (pos.x / _BGImage.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / _BGImage.rectTransform.sizeDelta.y);
+            pos.x /= bgImageSizeDelta.x;
+            pos.y /= (bgImageSizeDelta.y);
 
-            _inputVector = new Vector3 (pos.x * 2 + 1, 0, pos.y * 2 - 1);
-            _inputVector = (_inputVector.magnitude > 1.0f) ? _inputVector.normalized : _inputVector;
+            
+            //Let's assume that we are working with a 3D world. If necessary, change inputVector to use y-axis.
+            inputVector = new Vector3 (pos.x, 0, pos.y);
 
-            _joystickImage.rectTransform.anchoredPosition = new Vector3 (_inputVector.x * (_BGImage.rectTransform.sizeDelta.x / 2.5f),
-                _inputVector.z * (_BGImage.rectTransform.sizeDelta.y / 2.5f));
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            joystickImage.rectTransform.anchoredPosition = new Vector3 (inputVector.x * (bgImageSizeDelta.x / 2.5f),
+                inputVector.z * (bgImageSizeDelta.y / 2.5f));
+
+            //Get input vectors from joystick to use in movement
+            Horizontal();
+            Vertical();
         }
     }
 
@@ -46,32 +49,18 @@ public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler
 
     public virtual void OnPointerUp (PointerEventData pod)
     {
-        _inputVector = Vector3.zero;
-        _joystickImage.rectTransform.anchoredPosition = Vector3.zero;
+        inputVector = Vector3.zero;
+        joystickImage.rectTransform.anchoredPosition = Vector3.zero;
         JoystickPressed = false;
     }
 
     public float Horizontal ()
     {
-        if (_inputVector.x != 0)
-        {
-            return _inputVector.x;
-        }
-        else
-        {
-            return Input.GetAxis ("Horizontal");
-        }
+        return inputVector.x != 0 ? inputVector.x : Input.GetAxis ("Horizontal");
     }
     public float Vertical ()
     {
-        if (_inputVector.z != 0)
-        {
-            return _inputVector.z;
-        }
-        else
-        {
-            return Input.GetAxis ("Vertical");
-        }
+        return inputVector.z != 0 ? inputVector.z : Input.GetAxis ("Vertical");
     }
 
 }
